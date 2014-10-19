af95e9b23fdb9f84112c33a519467906ad55b37c
Jason Morris <jasonmorris@gmail.com>
2014-10-19 19:29:03 -0400

    Add gems to fetch

--- install-deps.sh
+++ install-deps.sh
@@ -1,11 +1,13 @@
-# https://rvm.io
-# rvm for the rubiess
+# Install rvm
 curl -L https://get.rvm.io | bash -s stable --autolibs=enable
 
 # Check gems are up to date without installing documentation
 sudo gem update --system --no-document
-# Install jekyll
+
+# Install gems
 sudo gem install jekyll
+sudo gem install github-pages
+sudo gem install sass
 
 # Set symlink for Sublime Text 2
 ln -s "/Applications/Sublime Text 3.app/Contents/SharedSupport/bin/subl" ~/bin/subl
