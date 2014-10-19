aa31d1d70b9b19025a3dabcd064c0a91f5efd443
Jason Morris <jasonmorris@gmail.com>
2014-10-19 18:37:14 -0400

    Switch to script from Brewfile, Caskfile

---
+++ homebrew.sh
@@ -0,0 +1,73 @@
+# Cloned from http://github.com/ahmedelgabri/dotfiles/
+# Homebrew taps
+TAPS=(
+    homebrew/dupes
+    homebrew/versions
+    caskroom/cask
+    caskroom/fonts
+    caskroom/versions
+)
+
+# Homebrew Formulas
+FORMULAS=(
+    coreutils
+    moreutils
+    findutils
+    gnu-sed --default-names
+    bash
+    bash-completion
+    wget --enable-iri
+    install vim --override-system-vi
+    homebrew/dupes/grep
+    homebrew/dupes/screen
+    install git
+    install node
+    install tree
+    caskroom/cask/brew-cask
+)
+
+# Homebrew casks
+CASKS=(
+    alfred
+    appcleaner
+    caffeine
+    diffmerge
+    dropbox
+    filezilla
+    firefox
+    flux
+    google-chrome
+    google-chrome-canary
+    imagealpha
+    imageoptim
+    integrity
+    iterm2
+    licecap
+    rdio
+    skype
+    spectacle
+    sublime-text3
+    the-unarchiver
+    tower
+    tunnelbear
+    vlc
+)
+
+#================================================================================
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+brew tap ${TAPS[@]} && brew install ${FORMULAS[@]}
+
+echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
+
+brew cask install --appdir="/Applications" ${CASKS[@]}
+
+echo "Put your license in Sublime and Tower"
+echo "Install 1Password, Reeder, Wunderlist, and Tweetbot"
+
+brew cleanup
