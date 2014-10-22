197f125714ffce7dd7720a18b4cae7b90db4f284
Jason Morris <jasonmorris@gmail.com>
2014-10-22 12:04:48 -0400

    Automate Package Control installation

--- sublime.sh
+++ sublime.sh
@@ -8,7 +8,11 @@ cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
 rm -r User
 ln -s ~/Dropbox/Apps/SublimeText/User
 
+# Download and install Package Control
+curl http://sublime.wbond.net/Package%20Control.sublime-package \
+  > ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package
+
 echo " "
-echo "Now open Sublime Text and install Package Control using the code from:"
-echo "https://sublime.wbond.net/installation"
+echo "Now open Sublime Text to allow Package Control"
+echo "to install packages specified in Dropbox."
 echo " "
