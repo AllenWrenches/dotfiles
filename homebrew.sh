413ef43db73e17ba21fe2befccf9f4bc31829c26
Jason Morris <jasonmorris@gmail.com>
2014-12-14 20:24:11 -0500

    Add calibre

--- homebrew.sh
+++ homebrew.sh
@@ -32,6 +32,7 @@ brew install tree
 brew cask install --appdir="/Applications" alfred
 brew cask install --appdir="/Applications" appcleaner
 brew cask install --appdir="/Applications" caffeine
+brew cask install --appdir="/Applications" calibre
 brew cask install --appdir="/Applications" diffmerge
 brew cask install --appdir="/Applications" dropbox
 brew cask install --appdir="/Applications" filezilla
