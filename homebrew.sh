cdede1e5741b02a216cba8a5c3836c2d167c0595
Jason Morris <jasonmorris@gmail.com>
2015-02-24 11:10:02 -0500

    Clean up

--- homebrew.sh
+++ homebrew.sh
@@ -75,7 +75,6 @@ brew cask install --appdir="/Applications" sublime-text3
 brew cask install --appdir="/Applications" superduper
 brew cask install --appdir="/Applications" the-unarchiver
 brew cask install --appdir="/Applications" tomahawk
-brew cask install --appdir="/Applications" torbrowser
 brew cask install --appdir="/Applications" tower
 brew cask install --appdir="/Applications" transmission
 brew cask install --appdir="/Applications" transmit
