6eabc6d0b2fc49adb1b00bfe41b4a25332a756c3
Jason Morris <jasonmorris@gmail.com>
2015-12-11 12:38:23 -0500

    Cleanup

--- homebrew.sh
+++ homebrew.sh
@@ -61,7 +61,6 @@ brew cask install --appdir="/Applications" integrity
 brew cask install --appdir="/Applications" iterm2
 brew cask install --appdir="/Applications" java
 brew cask install --appdir="/Applications" kaleidoscope
-brew cask install --appdir="/Applications" keepingyouawake
 brew cask install --appdir="/Applications" licecap
 brew cask install --appdir="/Applications" little-snitch
 brew cask install --appdir="/Applications" malwarebytes-anti-malware
