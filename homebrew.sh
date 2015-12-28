ff03134ffcf6026380b4a96c950e4bf8dc87ea73
Jason Morris <jasonmorris@gmail.com>
2015-12-28 12:23:29 -0500

    Add colour contrast analyser

--- homebrew.sh
+++ homebrew.sh
@@ -47,6 +47,7 @@ brew cask install --appdir="/Applications" alfred
 brew cask install --appdir="/Applications" appcleaner
 brew cask install --appdir="/Applications" bartender
 brew cask install --appdir="/Applications" caffeine
+brew cask install --appdir="/Applications" colour-contrast-analyser
 brew cask install --appdir="/Applications" dropbox
 brew cask install --appdir="/Applications" filezilla
 brew cask install --appdir="/Applications" firefox
