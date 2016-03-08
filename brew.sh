c1e30f00621cea6d9783bb419d0a05b68e5d3a21
Jason Morris <jasonmorris@gmail.com>
2016-03-07 21:13:27 -0500

    Remove unused installs, add cask installs

--- brew.sh
+++ brew.sh
@@ -19,87 +19,86 @@ brew upgrade --all
 brew install coreutils
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
-
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
-
 # Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-brew install homebrew/php/php56 --with-gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
 
 # Install other useful binaries.
-brew install ack
-brew install dark-mode
-#brew install exiv2
 brew install git
-brew install git-lfs
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
+brew install node
+brew install phantomjs
 brew install speedtest_cli
 brew install ssh-copy-id
-brew install testssl
-brew install tree
-brew install webkit2png
-brew install zopfli
+
+# Install applications with Cask
+brew cask install --appdir="/Applications" 1password
+brew cask install --appdir="/Applications" adobe-creative-cloud
+brew cask install --appdir="/Applications" acquia-dev-desktop
+brew cask install --appdir="/Applications" alfred
+brew cask install --appdir="/Applications" appcleaner
+brew cask install --appdir="/Applications" atom
+brew cask install --appdir="/Applications" bartender
+brew cask install --appdir="/Applications" caffeine
+brew cask install --appdir="/Applications" couleurs
+brew cask install --appdir="/Applications" colour-contrast-analyser
+brew cask install --appdir="/Applications" dropbox
+brew cask install --appdir="/Applications" firefox
+brew cask install --appdir="/Applications" flux
+brew cask install --appdir="/Applications" github-desktop
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" grandperspective
+brew cask install --appdir="/Applications" handbrake
+brew cask install --appdir="/Applications" handbrakecli
+brew cask install --appdir="/Applications" hazel
+brew cask install --appdir="/Applications" hipchat
+brew cask install --appdir="/Applications" imageoptim
+brew cask install --appdir="/Applications" integrity
+brew cask install --appdir="/Applications" iterm2
+brew cask install --appdir="/Applications" java
+brew cask install --appdir="/Applications" kaleidoscope
+brew cask install --appdir="/Applications" licecap
+brew cask install --appdir="/Applications" little-snitch
+brew cask install --appdir="/Applications" macdown
+brew cask install --appdir="/Applications" malwarebytes-anti-malware
+brew cask install --appdir="/Applications" micro-snitch
+brew cask install --appdir="/Applications" mp4tools
+brew cask install --appdir="/Applications" musicbrainz-picard
+brew cask install --appdir="/Applications" namechanger
+brew cask install --appdir="/Applications" nvalt
+brew cask install --appdir="/Applications" openemu
+brew cask install --appdir="/Applications" openscad
+brew cask install --appdir="/Applications" pandoc
+brew cask install --appdir="/Applications" ripit
+brew cask install --appdir="/Applications" sequel-pro
+brew cask install --appdir="/Applications" sitesucker
+brew cask install --appdir="/Applications" sketchup
+brew cask install --appdir="/Applications" skype
+brew cask install --appdir="/Applications" slack
+brew cask install --appdir="/Applications" slic3r
+brew cask install --appdir="/Applications" spotify
+brew cask install --appdir="/Applications" sublime-text3
+brew cask install --appdir="/Applications" superduper
+brew cask install --appdir="/Applications" teamviewer
+brew cask install --appdir="/Applications" textexpander
+brew cask install --appdir="/Applications" the-unarchiver
+brew cask install --appdir="/Applications" torbrowser
+brew cask install --appdir="/Applications" tower
+brew cask install --appdir="/Applications" transmission
+brew cask install --appdir="/Applications" transmit
+brew cask install --appdir="/Applications" tunnelbear
+brew cask install --appdir="/Applications" vagrant
+brew cask install --appdir="/Applications" virtualbox
+brew cask install --appdir="/Applications" zoomus
+
+# Install fonts with Cask
+brew cask install font-inconsolata
+brew cask install font-source-code-pro
+brew cask install font-source-sans-pro
+brew cask install font-source-serif-pro
+
+# Install useful QuickLook plugins with Cask
+brew cask install qlmarkdown quicklook-json qlimagesize
 
 # Remove outdated versions from the cellar.
 brew cleanup
+brew cask cleanup
