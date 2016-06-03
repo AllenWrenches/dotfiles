d2782f4fc0bf29aa40aa14270c817c20227111aa
Jason Morris <jasonmorris@gmail.com>
2016-06-03 18:14:30 -0400

    add qbserve

--- brew.sh
+++ brew.sh
@@ -58,6 +58,7 @@ brew cask install --appdir="/Applications" namechanger
 brew cask install --appdir="/Applications" nvalt
 brew cask install --appdir="/Applications" openemu
 brew cask install --appdir="/Applications" openscad
+brew cask install --appdir="/Applications" qbserve
 brew cask install --appdir="/Applications" pandoc
 brew cask install --appdir="/Applications" ripit
 brew cask install --appdir="/Applications" sequel-pro
